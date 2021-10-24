cdbb29d1f2fca3ab2a05761856c9c330c1ae5b50
Konrad Fuger <k.fuger@tuhh.de>
2021-10-24 14:56:13 +0200

    realize packet dropping on higher layer s.t. mac control packets survive

--- intairnet-link-layer/src/IntAirNetLinkLayer.cc
+++ intairnet-link-layer/src/IntAirNetLinkLayer.cc
@@ -100,7 +100,7 @@ void IntAirNetLinkLayer::initialize(int stage)
 
 
         rlcSubLayer = new Rlc(1600);
-        arqSubLayer = new SelectiveRepeatArq(MacId(address.getInt()), 100, 100);
+        arqSubLayer = new SelectiveRepeatArq(MacId(address.getInt()), 100, 100, par("per").doubleValue());
         macSubLayer = new MacLayer(MacId(address.getInt()), planning_horizon);
         phySubLayer = new PhyLayer(planning_horizon);
 
