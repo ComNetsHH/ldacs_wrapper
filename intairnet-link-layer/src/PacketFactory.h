fb3dada2824a2a4bcf84ec233c1d2549d90ac54d
Konrad Fuger <k.fuger@tuhh.de>
2021-10-26 11:07:37 +0200

    Display frame type in QtEnv

--- intairnet-link-layer/src/PacketFactory.h
+++ intairnet-link-layer/src/PacketFactory.h
@@ -13,12 +13,15 @@
 #include <L3Packet.hpp>
 
 using namespace inet;
+using namespace std;
 using namespace TUHH_INTAIRNET_MCSOTDMA;
 
 class PacketFactory {
 public:
     static IntAirNetLinkLayerPacket* fromL2Packet(L2Packet* source, uint64_t center_frequency);
     static L3Packet* fromInetPacket(Packet* source);
+    static string getPacketName(L2Packet* packet);
+
 };
 
 
