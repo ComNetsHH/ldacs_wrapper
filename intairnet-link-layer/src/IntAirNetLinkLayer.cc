c0f29a2887cbe92898825f8c92f517716db30304
Musab Ahmed <musab.ahmed@tuhh.de>
2022-02-10 12:54:29 +0100

    remove GPSR beacon form .cc and routing table module from .ned and clean the code

--- intairnet-link-layer/src/IntAirNetLinkLayer.cc
+++ intairnet-link-layer/src/IntAirNetLinkLayer.cc
@@ -54,7 +54,6 @@ void IntAirNetLinkLayer::initialize(int stage) {
         slotDuration = par("slotDuration");
         gpsrIsUsed = par("gpsrIsUsed").boolValue();
         arqIsUsed = par("arqIsUsed").boolValue();        
-	routingTable = getModuleFromPar<IRoutingTable>(par("routingTableModule"), this);
 	
         mcsotdma_statistics_map.clear();
         for (size_t i = 0; i < str_mcsotdma_statistics.size(); i++) {
@@ -443,23 +442,11 @@ void IntAirNetLinkLayer::onBeaconReceive(MacId origin_id, L2HeaderBeacon header)
     auto rcvdMacAddress = MacAddress(0x0AAA00000000ULL + (origin_id.getId() & 0xffffffffUL));
     L3Address rcvdIpAddress = arp->getL3AddressFor(rcvdMacAddress);
 
-    const auto& beacon = makeShared<GpsrBeaconModified>();
-    auto packet = new Packet();	
-    beacon->setAddress(rcvdIpAddress);
-    beacon->setPosition(rcvdPosition);
-    //TODO choose self address based on a new 'interfaces' parameter
-    L3Address ret = routingTable->getRouterIdAsGeneric();
-    // Probably unused. Just setting it for completeness
-    beacon->setChunkLength(B(rcvdIpAddress.getAddressType()->getAddressByteLength() + 8));
-
-    
     // @Musab, this code snippet will pass the beacon up, replace "MyNewGpsr" with you actual class name and make processBeacon a public function :)
     // Pass up beacon directly to gpsr (skipping NW layer)
     //GpsrModified* gpsr = getModuleFromPar<GpsrModified>(par("gpsrModule"), this);
     GpsrModified* gpsr = getModuleFromPar<GpsrModified>(par("gpsrModule"), this);
-    //gpsr->neighborPositionTable.setPosition(rcvdIpAddress, rcvdPosition);
-   gpsr->processBeaconMCSOTDMA(rcvdIpAddress, rcvdPosition);
-    //gpsr->processUdpPacket(udpPacket);
+    gpsr->processBeaconMCSOTDMA(rcvdIpAddress, rcvdPosition);
    
 }
 
